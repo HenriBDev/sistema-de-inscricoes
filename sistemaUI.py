012764857c9cad0ddfff5c202873e98082e08a06
HenriBDev <henribdev@gmail.com>
2021-09-25 22:08:48 -0300

    :sparkles: feat - Print Formatado agora suporta \n
    - Renomeada e modificada a função dividirLinhasECaracteres para quebrarLinhaPorLimite:
        • Agora essa função apenas quebra a linha recebida e retorna uma list com a quebra feita
    
    - Criação da função quebrarLinhaPorLF:
        • A função recebe uma linha com um ou mais caracteres de escape \n (Line Feed) e quebra a linha de acordo com suas posições para modificações posteriores
    
    - Pequenas modificações na função printFormatado para se adaptar às outras alterações.

--- sistemaUI.py
+++ sistemaUI.py
@@ -25,19 +25,20 @@ def stringParaList(stringParaConversao):
 def listParaString(listParaConversao):
     return "".join(listParaConversao)
 
-def printFormatado(texto = "", preenchimento = " ", alinhamento = "esquerda", repetir = 1, laterais = "", inverteLateral = True):
+def printFormatado(texto = "", alinhamento = "esquerda", repetir = 1, laterais = "", inverteLateral = True, preenchimento = " "):
 
     larguraTerminal = shutil.get_terminal_size()[0]
-    terminalPar = verificaSeDivisivelPor2(larguraTerminal)
     larguraTexto = larguraTerminal
+    terminalPar = verificaSeDivisivelPor2(larguraTerminal)
     existeTexto = bool(texto)
+    multiplasLinhas = verificaSeForList(texto)
     existeLaterais = verificaSeContemTexto(laterais)
     existePreenchimento = verificaSeContemTexto(preenchimento)
     textoFormatado = []
     textoQuebrado = []
+    textoQuebradoLF = []
 
     if repetir < 1: repetir = 1
-
     if alinhamento != "esquerda" and alinhamento != "direita" and alinhamento != "centralizado": alinhamento = "esquerda"
 
     if existePreenchimento:
@@ -58,11 +59,20 @@ def printFormatado(texto = "", preenchimento = " ", alinhamento = "esquerda", re
             larguraTexto = 2 if terminalPar else 1
         caracteresLaterais.reverse()
 
-    textoQuebrado = dividirLinhasECaracteres(texto, larguraTexto)
+    if not multiplasLinhas:
+        texto = [texto]
+    for linha in texto:
+        if "\n" in linha:
+            textoQuebradoLF.extend(quebrarLinhaPorLF(linha))
+        else:
+            textoQuebradoLF.append(linha)
+    for linha in (textoQuebradoLF if textoQuebradoLF else texto):
+        textoQuebrado.extend(quebrarLinhaPorLimite(linha, larguraTexto))
 
     qtdLinhas = len(textoQuebrado)
 
     for linhaAtual in range(qtdLinhas):
+        textoQuebrado[linhaAtual] = stringParaList(textoQuebrado[linhaAtual])
         qtdCaracteresTexto = len(textoQuebrado[linhaAtual])
         textoQuebrado[linhaAtual] = preencherLinha(qtdEspacosVazios = larguraTexto - qtdCaracteresTexto,
                                                    preenchimento = caracteresPreenchimento,
@@ -92,7 +102,7 @@ def printFormatado(texto = "", preenchimento = " ", alinhamento = "esquerda", re
     for r in range(repetir):
         for linhaAtual in range(qtdLinhas):
             textoFormatado.append(textoQuebrado[linhaAtual])
-            
+        
     for linha in textoFormatado:
         linhaEmString = listParaString(linha)
         print(linhaEmString, end = "")
@@ -113,26 +123,37 @@ def preencherLinha(qtdEspacosVazios, preenchimento, tipoPreenchimento = "central
         indice += 1
     return linha
 
-def dividirLinhasECaracteres(texto, larguraLinha):
-    textoQuebrado = []
+def quebrarLinhaPorLF(linha):
+    linhasQuebradas = []
+    qtdLF = linha.count("\n")
+    for linhaAdicional in range(qtdLF):
+        fimDaLinha = len(linha)
+        indiceFinalAnterior = linha.find("\n")
+        if indiceFinalAnterior > -1:
+            if indiceFinalAnterior > 0:
+                linhasQuebradas.append(linha[0 : indiceFinalAnterior])
+            else:
+                linhasQuebradas.append("")
+            if indiceFinalAnterior + 2 <= fimDaLinha:
+                linha = linha[indiceFinalAnterior + 1 : fimDaLinha]
+            else:
+                linha = ""
+    linhasQuebradas.append(linha)
+    return linhasQuebradas
+
+def quebrarLinhaPorLimite(linha, larguraLinha):
+    linhasQuebradas = []
     linhaPar = verificaSeDivisivelPor2(larguraLinha)
-    multiplasLinhas = verificaSeForList(texto)
-    if multiplasLinhas:
-        totalLinhas = len(texto)
+    caracteres = stringParaList(linha)
+    totalCaracteres = len(caracteres)
+    caracteresPar = verificaSeDivisivelPor2(totalCaracteres)
+    if totalCaracteres > larguraLinha:
+        totalDivisoes = totalCaracteres // larguraLinha + (1 if not caracteresPar and linhaPar else 0)
     else:
-        totalLinhas = 1
-    for linha in range(totalLinhas):
-        caracteres = stringParaList(texto[linha] if multiplasLinhas else texto)
-        totalCaracteres = len(caracteres)
-        caracteresPar = verificaSeDivisivelPor2(totalCaracteres)
-        if totalCaracteres > larguraLinha:
-            totalDivisoes = totalCaracteres // larguraLinha + (1 if not caracteresPar and linhaPar else 0)
-        else:
-            totalDivisoes = 1
-        for linhaDividida in range(totalDivisoes):
-            inicioLinha = linhaDividida * larguraLinha
-            finalLinha = inicioLinha + larguraLinha
-            linhaAtual = texto[linha][inicioLinha : finalLinha] if multiplasLinhas else texto[inicioLinha : finalLinha]
-            caracteresLinha = stringParaList(linhaAtual)
-            textoQuebrado.append(caracteresLinha)
-    return textoQuebrado
+        totalDivisoes = 1
+    for linhaDividida in range(totalDivisoes):
+        inicioDivisao = linhaDividida * larguraLinha
+        finalDivisao = inicioDivisao + larguraLinha
+        linhaAtual = linha[inicioDivisao : finalDivisao]
+        linhasQuebradas.append(linhaAtual)
+    return linhasQuebradas
