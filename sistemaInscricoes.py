d0b8906f0abb6553c19aab12badef491ef7a5cb9
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2021-10-02 20:03:26 -0300

    codigo ajustado para padroes pep-8

--- sistemaInscricoes.py
+++ sistemaInscricoes.py
@@ -1,6 +1,6 @@
 
 def inscreverCandidato(list, nome, email):
-	inscrito = {"nome": nome, "email" : email} # dicionário
+	inscrito = {"nome": nome, "email": email} # dicionário
 	list.append(inscrito)
 
 # H2
@@ -16,6 +16,7 @@ def classificarEmOrdemAlfabetica(list):
 	print("*** Lista de Inscritos em Ordem Alfabetica ***")
 	print(novaLista)
 
+
 # H6
 def alterarNome(list, email, novoNome):
 	for usuario in list:
@@ -23,23 +24,24 @@ def alterarNome(list, email, novoNome):
 			usuario["nome"] = novoNome
 
 
-
 def main():
 
 	candidatos = []
 
-	inscrito1 = {"nome" : "Gilberto De Melo Junior", "email" : "gilberto@email.com"}
-	inscrito2 = {"nome" : "Maria de Oliveira", "email" : "maria@email.com.br"}
-	inscrito3 = {"nome" : "Amanda da Silva", "email" : "jose@email.com"}
+	inscrito1 = {"nome": "Gilberto De Melo Junior", "email": "gilberto@email.com"}
+	inscrito2 = {"nome": "Maria de Oliveira", "email": "maria@email.com.br"}
+	inscrito3 = {"nome": "Amanda da Silva", "email": "jose@email.com"}
 
 
 	inscreverCandidato(candidatos, inscrito1["nome"], inscrito1["email"])
 	inscreverCandidato(candidatos, inscrito2["nome"], inscrito2["email"])
 	inscreverCandidato(candidatos, inscrito3["nome"], inscrito3["email"])
 	listarUsuarios(candidatos)
+
 	print('\n')
 	classificarEmOrdemAlfabetica(candidatos)
 	print('\n')
+
 	# Mudar nome
 	nome = "Maria Sanches"
 	alterarNome(candidatos, "maria@email.com.br", nome)
