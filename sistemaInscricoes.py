754369f2f41439f3ca3338c5fda2f162b1a7ed2c
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2021-10-05 16:03:44 -0300

    H1 - H6 finalizados

--- sistemaInscricoes.py
+++ sistemaInscricoes.py
@@ -1,28 +1,46 @@
-
-def inscreverCandidato(list, nome, email):
+#  H1
+def cadastrarUsuario(list: [], nome: str, email: str):
 	inscrito = {"nome": nome, "email": email} # dicionário
 	list.append(inscrito)
 
+
 # H2
-def listarUsuarios(list):
-	print("*** List a de Inscritos por Ordem de Cadastro.")
+def listarUsuarios(list: []):
 	for usuario in list:
 		print("Nome: " + usuario["nome"] + "	 " + "Email: " + usuario["email"])
 
 
+# H3
 def classificarEmOrdemAlfabetica(list):
-
 	novaLista = sorted(list, key=lambda k: k["nome"])
-	print("*** Lista de Inscritos em Ordem Alfabetica ***")
-	print(novaLista)
+	for usuario in novaLista:
+		print("Nome: " + usuario["nome"] + "	 " + "Email: " + usuario["email"])
+
+
+# H4
+def buscaPorNome(list: [], nome: str):
+	for usuario in list:
+		if usuario["nome"] == nome:
+			return True
+	return False
+
+
+# H5
+def deletarUsuario(list: [], email: str):
+	for usuario in list:
+		if usuario["email"] == email:
+			list.pop(usuario)
+			return True
+	return False
 
 
 # H6
-def alterarNome(list, email, novoNome):
+def alterarNome(list: [], email: str, novoNome: str):
 	for usuario in list:
 		if usuario["email"] == email:
 			usuario["nome"] = novoNome
-
+			return True
+	return False
 
 def main():
 
@@ -33,19 +51,26 @@ def main():
 	inscrito3 = {"nome": "Amanda da Silva", "email": "jose@email.com"}
 
 
-	inscreverCandidato(candidatos, inscrito1["nome"], inscrito1["email"])
-	inscreverCandidato(candidatos, inscrito2["nome"], inscrito2["email"])
-	inscreverCandidato(candidatos, inscrito3["nome"], inscrito3["email"])
-	listarUsuarios(candidatos)
+	cadastrarUsuario(candidatos, inscrito1["nome"], inscrito1["email"])
+	cadastrarUsuario(candidatos, inscrito2["nome"], inscrito2["email"])
+	cadastrarUsuario(candidatos, inscrito3["nome"], inscrito3["email"])
 
+	print('Lista de usuários por ordem de cadastro:')
+	listarUsuarios(candidatos)
 	print('\n')
+
+	print('Lista de usuários em ordem alfabética:')
 	classificarEmOrdemAlfabetica(candidatos)
 	print('\n')
 
-	# Mudar nome
 	nome = "Maria Sanches"
-	alterarNome(candidatos, "maria@email.com.br", nome)
+	resultado = alterarNome(candidatos, "maria@email.com.br", nome)
+	if resultado == True:
+		print("Nome alterado.")
+	else:
+		print("Usuario nao encontrado.")
 	listarUsuarios(candidatos)
 
 
-main()
+if __name__ == "__main__":
+	main()
