09cc2e48da4c35c88dba9ff9c6b59becaf600bec
HenriBDev <henribdev@gmail.com>
2021-10-19 10:29:37 -0300

    :sparkles: feat - Função de limpar terminal
    - Adicionada função limparTela que verifica o sistema operacional atual e executa o comando de limpeza do terminal (funciona com Windows, Linux e Mac)

--- sistemaUI.py
+++ sistemaUI.py
@@ -1,14 +1,19 @@
 import shutil
 import os
+import platform
 
 def limparTela():
-    os.system("CLS")
+    sistemaOp = platform.system()
+    if (sistemaOp == "Windows"):
+        os.system("CLS")
+    elif (sistemaOp == "Linux" or sistemaOp == "Darwin"):
+        os.system("clear")
 
 def verificaSeContemTexto(dado):
     return bool(str(dado).strip())
 
 def verificaSeDivisivelPor2(num):
-    return num % 2 == 0
+    return (num % 2 == 0)
 
 def verificaSeForList(dado):
     return isinstance(dado, (list))
@@ -28,7 +33,7 @@ def listParaString(listParaConversao):
 def printFormatado(tamanhoLinha = shutil.get_terminal_size()[0], texto = "", alinhamento = "esquerda", repetir = 1, laterais = "", inverteLateral = True, separaLateral = False, preenchimento = " "):
 
     tamanhoTerminal = shutil.get_terminal_size()[0]
-    if tamanhoLinha > tamanhoTerminal:
+    if (tamanhoLinha > tamanhoTerminal):
         tamanhoLinha = tamanhoTerminal
     tamanhoTexto = tamanhoLinha
     linhaPar = verificaSeDivisivelPor2(tamanhoLinha)
@@ -40,8 +45,8 @@ def printFormatado(tamanhoLinha = shutil.get_terminal_size()[0], texto = "", ali
     textoQuebrado = []
     textoQuebradoLF = []
 
-    if repetir < 1: repetir = 1
-    if alinhamento != "esquerda" and alinhamento != "direita" and alinhamento != "centralizado": alinhamento = "esquerda"
+    if (repetir < 1): repetir = 1
+    if (alinhamento != "esquerda" and alinhamento != "direita" and alinhamento != "centralizado"): alinhamento = "esquerda"
 
     if existePreenchimento:
         caracteresPreenchimento = stringParaList(str(preenchimento))
@@ -52,10 +57,10 @@ def printFormatado(tamanhoLinha = shutil.get_terminal_size()[0], texto = "", ali
         caracteresLaterais = stringParaList(laterais)
         qtdCaracteresLaterais = len(caracteresLaterais)
         tamanhoTexto -= qtdCaracteresLaterais * 2 + (2 if separaLateral else 0) 
-        if tamanhoTexto <= 0:
+        if (tamanhoTexto <= 0):
             caracteresAMais = 1
             caracteresLaterais.pop()
-            if tamanhoTexto < 0:
+            if (tamanhoTexto < 0):
                 caracteresAMais += (tamanhoTexto * -1) // 2 + 1
                 del caracteresLaterais[qtdCaracteresLaterais - caracteresAMais + 1 : qtdCaracteresLaterais]
             tamanhoTexto = 2 if linhaPar else 1
@@ -114,13 +119,13 @@ def preencherLinha(qtdEspacosVazios, preenchimento, tipoPreenchimento = "central
     indice = 0
     caracterAtual = 1
     qtdCaracteresPreenchimento = len(preenchimento)
-    while caracterAtual <= qtdEspacosVazios:
-        if indice >= qtdCaracteresPreenchimento:
+    while (caracterAtual <= qtdEspacosVazios):
+        if (indice >= qtdCaracteresPreenchimento):
             indice = 0
-        if tipoPreenchimento == "direita" or tipoPreenchimento == "centralizado" and caracterAtual <= qtdEspacosVazios:
+        if (tipoPreenchimento == "direita" or tipoPreenchimento == "centralizado" and caracterAtual <= qtdEspacosVazios):
             linha.insert(0, preenchimento[indice])
             caracterAtual += 1
-        if tipoPreenchimento == "esquerda" or tipoPreenchimento == "centralizado" and caracterAtual <= qtdEspacosVazios:
+        if (tipoPreenchimento == "esquerda" or tipoPreenchimento == "centralizado" and caracterAtual <= qtdEspacosVazios):
             linha.append(preenchimento[indice])
             caracterAtual += 1
         indice += 1
@@ -132,12 +137,12 @@ def quebrarLinhaPorLF(linha):
     for linhaAdicional in range(qtdLF):
         fimDaLinha = len(linha)
         indiceFinalAnterior = linha.find("\n")
-        if indiceFinalAnterior > -1:
-            if indiceFinalAnterior > 0:
+        if (indiceFinalAnterior > -1):
+            if (indiceFinalAnterior > 0):
                 linhasQuebradas.append(linha[0 : indiceFinalAnterior])
             else:
                 linhasQuebradas.append("")
-            if indiceFinalAnterior + 2 <= fimDaLinha:
+            if (indiceFinalAnterior + 2 <= fimDaLinha):
                 linha = linha[indiceFinalAnterior + 1 : fimDaLinha]
             else:
                 linha = ""
@@ -148,7 +153,7 @@ def quebrarLinhaPorLimite(linha, tamanhoLinha):
     linhasQuebradas = []
     caracteres = stringParaList(linha)
     totalCaracteres = len(caracteres)
-    if totalCaracteres > tamanhoLinha:
+    if (totalCaracteres > tamanhoLinha):
         totalDivisoes = totalCaracteres // tamanhoLinha + (1 if totalCaracteres % tamanhoLinha > 0 else 0)
     else:
         totalDivisoes = 1
